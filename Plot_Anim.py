ac14a6c717df09f3792a02691e22c74e403217ee
Kalkberg <petr.v.yakovlev@gmail.com>
2017-01-26 12:42:14 -0700

    Added options and updated aesthetics.
    - Added option to plot in Lambert Conformal Conic to code
    - Fixed line weights to remove distraction from data

--- Plot_Anim.py
+++ Plot_Anim.py
@@ -4,7 +4,7 @@ Created on Tue Jan 24 11:29:59 2017
 
 Script to plot Lat Long data over a time interval.
 
-Made to plot volcanic data from North America.
+Made and currently set to plot volcanic data from North America.
 
 @author: Kalkberg
 """
@@ -18,32 +18,34 @@ import os
 from mpl_toolkits.basemap import Basemap
 
 
-## Create function describing usage and terminating program for errors
-#def error():
-#    print("Usage: python3 Plot_Anim.py working_dir data.csv Output")
-#    print("Where working_dir is the full path to the data")
-#    print("e.g. C:/users/animation")
-#    print("data.csv contains comma delimited files of age, lat, and long")
-#    print("Output is the name of the pdf map to generate from data")
-#    sys.exit()
-#
-## Check number of arguments and import, if not three, return error
-#if len(sys.argv) == 4:
-#    # Import directory to work on and satellite type from input arguments
-#    workdir = sys.argv[1]
-#    data = sys.argv[2]
-#    output = sys.argv[3]
-#    print("Current working directory is set to %s" % workdir)
-#    print("Data file set to %s" % data)
-#    print("Output file set to %s" % output)
-#else:
-#    print("Error: Wrong number of input arguments!")
-#    error()
-#    
-## Move to working directory
-#os.chdir(workdir)
+# Create function describing usage and terminating program for errors
+def error():
+    print("Usage: python3 Plot_Anim.py working_dir data.csv Output")
+    print("Where working_dir is the full path to the data")
+    print("e.g. C:/users/animation")
+    print("data.csv contains comma delimited files of age, lat, and long")
+    print("Output is the name of the pdf map to generate from data")
+    sys.exit()
 
-# Input paramaters for debuging
+# Check number of arguments and import, if not three, return error
+if len(sys.argv) == 4:
+    # Import directory to work on and satellite type from input arguments
+    workdir = sys.argv[1]
+    data = sys.argv[2]
+    output = sys.argv[3]
+    print("Current working directory is set to %s" % workdir)
+    print("Data file set to %s" % data)
+    print("Output file set to %s" % output)
+else:
+    print("Error: Wrong number of input arguments!")
+    error()
+    
+# Move to working directory
+os.chdir(workdir)
+#
+## Input paramaters for debuging
+#data = 'NAM_Ig3.csv'
+#output = 'NAM_Ig4'
 
 # Read in data, cut out headers and redistribute
 data = np.genfromtxt(data, delimiter=',')
@@ -60,6 +62,7 @@ age, lat, long = data[:,0], data[:,1], data[:,2]
 # Parameter that affects length of animation
 p = 6 # Controlls length of animation
 tred = 1 # Controlls how long to keep red dots
+tblack = 10 # Controlls how long to keep black dots
 
 # Set up figure and background
 fig = plt.figure()
@@ -69,19 +72,23 @@ ax = fig.add_subplot(111)
 #ax.set_ylim([lat.min() - 0.1*lat.min(), lat.max() + 0.1*lat.max()])
 ax.set_xlim([-128,-101])
 ax.set_ylim([29,50])
+#m = Basemap(width=2300000,height=2400000,
+#            rsphere=(6378137.00,6356752.3142),\
+#            resolution='i',area_thresh=1000.,projection='lcc',\
+#            lat_1=35.,lat_2=45,lat_0=40,lon_0=-113.)
 m = Basemap(projection='merc',llcrnrlat=29,urcrnrlat=50,\
-            llcrnrlon=-128,urcrnrlon=-101,lat_ts=40,resolution='l')
+            llcrnrlon=-128,urcrnrlon=-101,lat_ts=40,resolution='i')
 #m = Basemap(projection='merc',
 #            llcrnrlat=(lat.min() - 0.1*lat.min()),
 #            urcrnrlat=(lat.max() + 0.1*lat.max()),
 #            llcrnrlon=(long.min() - 0.1*long.min()),
 #            urcrnrlon=(lat.max() + 0.1*lat.max()),
 #            lat_ts=lat.mean(),resolution='l')
-m.drawcoastlines()
-m.drawparallels(np.arange(30.,50.,5.), labels=[1, 1, 0, 0])
+m.drawcoastlines(linewidth=0.5)
 m.drawcountries(linewidth=0.5, linestyle='solid', color='k')
 m.drawstates(linewidth=0.5, linestyle='solid', color='k')
-m.drawmeridians(np.arange(-125.,105.,10.), labels=[0, 0, 0, 1])
+m.drawparallels(np.arange(30.,50.,5.), linewidth=.75, labels=[1, 1, 0, 0])
+m.drawmeridians(np.arange(-125.,105.,10.), linewidth=.75, labels=[0, 0, 0, 1])
 m.drawmapboundary(fill_color='white')
 plt.title('Western US Igneous Activity')
 
