2bd15ed74ba3cf67d94bcbfff46f7e1e9090e7f7
Kalkberg <petr.v.yakovlev@gmail.com>
2017-03-01 15:41:06 -0700

    Made more progress

--- Point_Poly.py
+++ Point_Poly.py
@@ -28,7 +28,7 @@ PolysFile = 'Polys.csf'
 PolysName = ('Mojave','NV','NVID','NVUT','ORID','ORNV')
 AgeMin = 0
 AgeMax = 36
-AgeInt = 0,5
+AgeInt = 0.5
 Output = 'Volc'
 
 # Read in data, cut out headers and redistribute to variables
@@ -42,30 +42,50 @@ PolyNo, PolyLat, PolyLong = Polys[:,0], Polys[:,1], Polys[:,2]
 
 # Create empty list of points in each polygon
 # Columns for each polygon are n=AgeMin, n+1=AgeMax, n+2=Lat, n+3=Long
-PointList = np.zeros([0,4*(PolyNo.max()+1)])
+PointList = []
+
+# Create list of age bins
+AgeBins = np.linspace(AgeMin,AgeMax,(AgeMax/AgeInt)+1)
 
 # Append points to an array of data for each polygon
 for i in range(0,PolyNo.max()):
     Polygon = np.zeros([0,2]) # Empty polygon for next step
     # Create array defining polygon
-    for j in range(0, len(PolyNo)):
+    for j in range(0, len(PolyNo)-1):
         if PolyNo == i:
             Polygon = np.append(Polygon,[[PolyLong[j],PolyLat[j]]],axis=0)
     
     PolyPath = path.Path(Polygon) # Create path using matplotlib.path
-    PointListPoly = np.zeros([0,4]) # Empty list of points in a set polygon
+    PointAgeMinPoly = [] # Empty list of points in a set polygon
+    PointAgeMaxPoly = []
+    PointLatPoly = []
+    PointLongPoly = []
     
-    # Check if points are in the polygon, if so append to list 
+    # Check if points are in the polygon, if so append to list, can also be done as list comprehension
     for k in range(0,len(PointLat)):
         if PolyPath.contains_point((PointLong[k],PointLat[k])) == True:
-            PointListPoly = np.append(PointListPoly,[[PointAgeMin[k], 
-                            PointAgeMax[k], PointLat[k], PointLong[k]]])
+            PointAgeMinPoly.append(PointAgeMin[k])
+            PointAgeMinPoly.append(PointAgeMax[k])
+            PointLatPoly.append(PointLat[k])
+            PointLongPoly.append(PointLong[k])
     
-    # Append to master list of data for each polygon 
-    PointList = np.append(PointList,PointListPoly,axis=1)
+    # Append to master list of data for each polygon
+    PointList.extend([PointAgeMinPoly,PointAgeMaxPoly, \
+                      PointLatPoly,PointLongPoly]) 
 
-
-
+# Make plots of sample numbers for each plot
+for m in range(0,PolyNo.max()):
+    
+    AgeRandDist = [] # Create empty list of age counts
+    for _ in range(10^5): #Repeat 10^5 times to get good PDFs n each bin
+        AgeRand = [] # Set list of generated ages to zero
+        for n in range(0,len(PointList[m*4])): #for each point found in the polygon
+            # Randomly generate numbers for each age interval
+            Age=np.random.uniform(PointList[m*4][n],PointList[m*4+1][n],1)
+            Age.tolist()
+            AgeRand.append(Age)
+        # Count number of points in each age interval and save
+        for p in AgeBins:
                                         
                                         
                                                 
