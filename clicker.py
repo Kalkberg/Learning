5dd607ff53b1e339248b499321e3bf3da3580207
Kalkberg <petr.v.yakovlev@gmail.com>
2017-08-18 11:39:43 -0600

    Created clicker script to use with arc

---
+++ clicker.py
@@ -0,0 +1,40 @@
+# -*- coding: utf-8 -*-
+"""
+Click on keypress
+
+@author: Kalkberg
+"""
+import time
+from pynput import keyboard
+from pynput.mouse import Button, Controller
+
+mouse = Controller()
+
+def keepclicking():
+    mouse.click(Button.left, 1)
+    time.sleep(.4)
+
+def on_press(key):
+    try:
+        if format(key.char) == 'w': #single click on w
+            mouse.press(Button.left)
+            mouse.release(Button.left)
+        elif format(key.char) == 'e': #double click on e
+            mouse.click(Button.left, 2)
+        elif format(key.char) == 'r': #repeatedly click on r
+            for i in range(8):
+                keepclicking()
+#        elif format(key.char) == 't':
+    except AttributeError:
+        pass
+
+def on_release(key):
+    if key == keyboard.Key.esc:
+        # Stop listener
+        return False
+
+# Collect events until released
+with keyboard.Listener(
+        on_press=on_press,
+        on_release=on_release) as listener:
+    listener.join()
