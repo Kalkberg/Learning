6fcff933d67de29545f2817c1db6f439a4eaf067
Kalkberg <petr.v.yakovlev@gmail.com>
2017-10-21 12:22:48 -0600

    Refined code so it stops when video ends

--- frame_cut.py
+++ frame_cut.py
@@ -8,15 +8,18 @@ Author:
 """
 import cv2
 
-video = "testvid.mp4"
-n = 150
+video = "cup.mp4"
+n = 30
 
 vidcap = cv2.VideoCapture(video)
+
+# count number of frames in video
+length = int(vidcap.get(cv2.CAP_PROP_FRAME_COUNT))
 
 # Set counter to zero
 count = 0
 
-while(True):
+while count < length:
     # Capture frame-by-frame
     ret, frame = vidcap.read()
     
@@ -25,8 +28,4 @@ while(True):
     
     # If frame number divisible by n, save it as a jpg
     if count % n == 0:
-        cv2.imwrite(video[:-4] + "_frame%d.jpg" % count, frame) 
-    
-    # Break when video is over
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
+        cv2.imwrite(video[:-4] + "_frame%d.jpg" % count, frame)
